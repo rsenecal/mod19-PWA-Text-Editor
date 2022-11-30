880274bd848be1d2bb468c333a9de03951d8d02c
rsenecal <roodysenecal@gmail.com>
2022-11-30 18:04:11 -0500

    Heroku Headache 4

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { GenerateSW } = require('workbox-webpack-plugin');
 
 // const { InjectManifest } = require('workbox-webpack-plugin');
